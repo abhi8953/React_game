cbbdc569138bf8cfcdd61540f67604fce888afe8
abhi893 <abhishek98.ac@gmail.com>
2022-07-14 08:59:03 +0530

    added constructors in components

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
     render() {
       return (
-        <button className="square" onClick={()=> {console.log('click');}}>
-          {this.props.value}
+        <button className="square" 
+        onClick={()=> this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      }
+    }
     renderSquare(i) {
-      return <Square value={i} />;
+      return <Square value={this.state.squares[i]} 
+      onClick = {() => this.handleClick(i)}
+      />;
     }
   
     render() {
